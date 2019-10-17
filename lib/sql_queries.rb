b84a4083f9f4967640813ec7fb2f7b0331b97cfc
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-17 20:11:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -53,5 +53,5 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   FROM projects
   JOIN pledges
   ON projects.id = pledges.project_id
-  HAVING projects.category = 'books'"
+  GROUP BY projects.category = 'books'"
 end
