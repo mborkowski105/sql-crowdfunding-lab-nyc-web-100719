6dc521f72b9b43a19f95c3b3668c412dab098dac
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-17 19:23:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT project.title, SUM(pledges.amount)
   FROM projects
-  LEFT INNER JOIN pledges
+  LEFT JOIN pledges
   ON projects.id = pledges.project_id
   GROUP BY project.title"
 end
